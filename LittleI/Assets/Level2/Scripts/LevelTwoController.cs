725584c0196597b00e558f535beb0cf2ccf07d76
L1ZI7 <zhangshenrui1017@gmail.com>
2024-01-26 19:08:25 +0800

    add prefabs

--- LittleI/Assets/Level2/Scripts/LevelTwoController.cs
+++ LittleI/Assets/Level2/Scripts/LevelTwoController.cs
@@ -13,18 +13,18 @@ public class LevelTwoController : MonoBehaviour
     private List<List<int>> _snapShotsList = new List<List<int>>();
     private async void Start()
     {
-        lineController.GenerateLine(_levelTwoConfig);
-        List<int> snapShots1 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot1).ToList();
-        List<int> snapShots2 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot2).ToList();
-        List<int> snapShots3 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot3).ToList();
-        _snapShotsList.Add(snapShots1);
-        _snapShotsList.Add(snapShots2);
-        _snapShotsList.Add(snapShots3);
-        
-        foreach (var snapshot in _snapShotsList)
-        {
-            lineController.RefreshLineCells(snapshot);
-            await UniTask.Delay(TimeSpan.FromSeconds(5), ignoreTimeScale: false);
-        }
+        // lineController.GenerateLine(_levelTwoConfig);
+        // List<int> snapShots1 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot1).ToList();
+        // List<int> snapShots2 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot2).ToList();
+        // List<int> snapShots3 = _levelTwoConfig.LevelTwoConfigEntities.Select(x => x.snapshot3).ToList();
+        // _snapShotsList.Add(snapShots1);
+        // _snapShotsList.Add(snapShots2);
+        // _snapShotsList.Add(snapShots3);
+        //
+        // foreach (var snapshot in _snapShotsList)
+        // {
+        //     lineController.RefreshLineCells(snapshot);
+        //     await UniTask.Delay(TimeSpan.FromSeconds(5), ignoreTimeScale: false);
+        // }
     }
 }
